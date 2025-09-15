d6a36cb7ca74f69222845735ad68f2808ca6d4ab
Felix Sun <felixsun0414@gmail.com>
2025-09-15 00:16:11 -0500

    quick change with in memory map as db

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,20 @@
+import string
+import random
+import validators
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from fastapi.responses import RedirectResponse
+from fastapi.responses import JSONResponse, RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
+from urllib.parse import urlparse
 
 app = FastAPI()
 
-# Behold, the database!
-db = {"abc123": "https://example.com"}
+# Behold, the in memory storage!
+code_to_url = {}
+url_to_code = {}
+
+baseUrl = "http://localhost:5000"
+codeSources = string.ascii_letters + string.digits
 
 origins = ["http://localhost:3000"]
 
@@ -18,15 +26,46 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# a short code with 7 chars (mix of alphabetic charactors and numerals) is able to cover 62^7 URLs.
+def generate_code(length: int = 7) -> str:
+    return ''.join(random.choices(codeSources, k=length))
+
 class URLRequest(BaseModel):
     url: str
 
 @app.post("/api/shorten")
 def shorten_url(request: URLRequest):
-    return {"short_code": "abc123", "short_url": "http://localhost:5000/abc123", "original_url": "http://example.com" }
+    origin_url = request.url
+    rsp_status = 200
+    code = ""
+
+    print(f"url: {origin_url}")
+    if not validators.url(origin_url):
+        print("invalid URL")
+        raise HTTPException(status_code=400, detail="invalid URL")
+    elif origin_url in url_to_code:
+        # if this URL already exists, retrieve the shorten code
+        code = url_to_code[origin_url]
+        print(f"found code: {code}")
+    else:
+        # this is a new URL, create a unique shorten code
+        code = generate_code()
+        while code in code_to_url:
+            code = generate_code()
+        code_to_url[code] = origin_url
+        url_to_code[origin_url] = code
+
+        rsp_status = 201
+        print(f"new code created: {code}")
+
+    return JSONResponse(
+        status_code=rsp_status,
+        content={ "short_code": code, "short_url": f"{baseUrl}/{code}", "origin_url": origin_url }
+    )
 
 @app.get("/{short_code}")
 def redirect(short_code: str):
-    if short_code in db:
-        return RedirectResponse(url=db[short_code])
+    print(f"short_code: {short_code}")
+    if short_code in code_to_url:
+        return RedirectResponse(url=code_to_url[short_code], status_code=302)
     raise HTTPException(status_code=404, detail="Short code not found")
